09c717e5bfc9bd5e53067447d26f942b52bad15a
AinhoaFE <47415302+AinhoaFE@users.noreply.github.com>
2022-12-14 17:06:31 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
  //host: env('HOST', '0.0.0.0'),
-  url: env('PUBLIC_URL', 'https://strapionlypadel.herokuapp.com/'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  url: env('https://strapionlypadel.herokuapp.com/'),
+  //port: env.int('PORT', 1337),
+ // app: {
+  //  keys: env.array('APP_KEYS'),
+ // },
 });
